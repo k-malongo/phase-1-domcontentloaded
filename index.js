cbf9d6896478ffef0f9a3bb0803609e9ae087bb4
Kelvin <vokemalosh32@gmail.com>
2022-06-14 21:02:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("text").innerHTML = document.getElementById("text").innerHTML.replace('JavaScript is so cool. It lets me add text to my page programmatically.', "This is really cool!");
+
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
